c4e8171f2cd9550ff11e8b20228c34fc1e6b1d1d
Jess Moore <104534587+jmychalm13@users.noreply.github.com>
2024-08-26 18:48:31 -0500

    added order create action and test passing also updated fixtures and added user_id column and association

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,4 +3,28 @@ class OrdersController < ApplicationController
     @orders = Order.all
     render :index
   end
+
+  def create
+    @order = Order.create(
+      user_id: current_user.id,
+      customer_email: current_user.email,
+      status: params[:status],
+      customer_name: current_user.name,
+      total_price: params[:total_price],
+      order_date: DateTime.now,
+      payment_status: false,
+    )
+    if @order.persisted?
+      render :show
+    else
+      render json: { errors: @order.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
 end
+# json.id order.id
+# json.customer_email order.customer_email
+# json.status order.status
+# json.customer_name order.customer_name
+# json.total_price order.total_price
+# json.order_date order.order_date
+# json.payment_status order.payment_status
--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -1,11 +1,40 @@
 require "test_helper"
 
 class OrdersControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = User.create(
+      name: "Jane",
+      email: "jane@test.com",
+      password: "password",
+      password_confirmation: "password"
+    )
+    post "/sessions.json", params: {email: "jane@test.com", password: "password"}
+    data = JSON.parse(response.body)
+    @jwt = data["jwt"]
+  end
+
   test "index" do
     get "/orders.json"
     assert_response 200
 
     data = JSON.parse(response.body)
     assert_equal Order.count, data.length
+  end
+
+  test "create" do
+    assert_difference "Order.count", 1 do
+      post "/orders.json", params: {
+        user_id: @user.id,
+        customer_email: @user.email,
+        status: :pending,
+        customer_name: @user.name,
+        total_price: 9.99,
+        order_date: DateTime.now,
+        payment_status: false,
+      }, headers: {
+        "Authorization" => "Bearer #{@jwt}"
+      }
+      assert_response 200
+    end
   end
 end
