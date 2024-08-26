0185de0bb39888e94515b405fc4cf44e658cdb1d
Jess Moore <104534587+jmychalm13@users.noreply.github.com>
2024-08-26 07:39:22 -0500

    menu_item create method added with test

--- app/controllers/menu_items_controller.rb
+++ app/controllers/menu_items_controller.rb
@@ -3,4 +3,15 @@ class MenuItemsController < ApplicationController
     @menu_items = MenuItem.all
     render :index
   end
+
+  def create
+    @menu_item = MenuItem.create(
+      name: params[:name],
+      description: params[:description],
+      price: params[:price],
+      availability: params[:availability],
+      category: params[:category],
+    )
+    render :show
+  end
 end
--- test/controllers/menu_items_controller_test.rb
+++ test/controllers/menu_items_controller_test.rb
@@ -8,4 +8,17 @@ class MenuItemsControllerTest < ActionDispatch::IntegrationTest
     data = JSON.parse(response.body)
     assert_equal MenuItem.count, data.length
   end
+
+  test "create" do
+    assert_difference "MenuItem.count", 1 do
+      post "/menu_items.json", params: {
+        name: "bread sticks",
+        category: :appetizer,
+        price: 8.99,
+        description: "garlic bread",
+        availability: true,
+      }
+      assert_response 200
+    end
+  end
 end
