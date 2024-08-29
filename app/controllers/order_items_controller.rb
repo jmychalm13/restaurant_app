24bcaeffc30d3811dd5d05398d77428d373044f5
Jess Moore <104534587+jmychalm13@users.noreply.github.com>
2024-08-29 16:11:19 -0500

    added create method for order_item and test setup and create test

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -3,4 +3,21 @@ class OrderItemsController < ApplicationController
     @order_items = OrderItem.all
     render :index
   end
+
+  def create
+    @order_item = OrderItem.create(
+      order_id: params[:order_id],
+      menu_item_id: params[:menu_item_id],
+      quantity: params[:quantity],
+      unit_price: params[:unit_price]
+    )
+    if @order_item.save
+      render :show
+    else
+      render json: {errors: @order_item.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
 end
+
+
+
--- test/controllers/order_items_controller_test.rb
+++ test/controllers/order_items_controller_test.rb
@@ -1,11 +1,51 @@
 require "test_helper"
 
 class OrderItemsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = User.create(
+      name: "Jane",
+      email: "jane@test.com",
+      password: "password",
+      password_confirmation: "password"
+    )
+    post "/sessions.json", params: {email: "jane@test.com", password: "password"}
+    data = JSON.parse(response.body)
+    @jwt = data["jwt"]
+    @order = Order.create(
+      user_id: @user.id,
+      customer_email: @user.email,
+      status: :pending,
+      customer_name: @user.name,
+      total_price: 9.99,
+      order_date: DateTime.now(),
+      payment_status: false,
+    )
+    @menu_item = MenuItem.create(
+      name: "bread sticks",
+      category: :appetizer,
+      price: 8.99,
+      description: "garlic bread",
+      availability: true,
+    )
+  end
   test "index" do
     get "/order_items.json"
     assert_response 200
 
     data = JSON.parse(response.body)
     assert_equal OrderItem.count, data.length
+  end
+
+  test "create" do
+    assert_difference "OrderItem.count", 1 do
+      post "/order_items.json", params: {
+        order_id: @order.id,
+        menu_item_id: @menu_item.id,
+        quantity: 2,
+        unit_price: @menu_item.price
+      }, headers: {
+        "Authorization" => "Bearer #{@jwt}"
+      }
+    end
   end
 end
